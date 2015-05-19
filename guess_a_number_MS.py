6aff7691efe340d13c0227374600dd7024ab2da7
PoolUser <biostudent209@fredda.biologie.hu-berlin.de>
2015-05-19 16:26:24 +0200

    zweite Version

--- guess_a_number_MS.py
+++ guess_a_number_MS.py
@@ -6,15 +6,20 @@ solved = False            #Loesung gefunden oder nicht
 print "Hello, let's play a game!"
 
 while solved == False :
-	eingabe = int(raw_input("please insert an integer between 0 and 100:"))
-	counter = counter + 1
-	if (eingabe == correct):
-		print ('Yippie, you won! You tried ',counter,' times')
-		solved = True
-	elif (eingabe < correct):
-		print ('Sorry, your number was too low')
+	eingabe = int(raw_input("Please insert an integer between 0 and 100:"))
+	if eingabe > 100:
+		print ('Your number is too high')
+	elif eingabe < 0:
+		print ('Your number is too low')
 	else:
-		print ('Sorry, your number was too high')
+		counter = counter + 1
+		if (eingabe == correct):
+			print 'Yippie, you won! You tried {} times'.format(counter)
+			solved = True
+		elif (eingabe < correct):
+			print ('Sorry, your number was too low')
+		else:
+			print ('Sorry, your number was too high')
 
 
 
